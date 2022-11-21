bb073a4b0ab4a42c9b6ca14002b5638e2756a539
this-is-yello <yello_3617@naver.com>
2022-11-22 01:05:51 +0900

    221122 in Laptop

--- flutter_instagram_clone/lib/main.dart
+++ flutter_instagram_clone/lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(
+          iconTheme: IconThemeData(color: Colors.blue),
+          appBarTheme: AppBarTheme(
+              color: Colors.grey,
+              actionsIconTheme: IconThemeData(color: Colors.blue)),
+          textTheme: TextTheme(bodyText2: TextStyle(color: Colors.red))),
+      home: MyApp()));
+}
+
+var a = TextStyle();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [Icon(Icons.star)],
+      ),
+      body: Text(
+        '안녕',
+        style: a,
+      ),
+    );
+  }
+}
